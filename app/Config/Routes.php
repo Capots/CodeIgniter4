328ce3bf203713f0a69e6ebfe0d1bb8c84eeffd9
Joseph Capote <capotejoseph@gmail.com>
2023-12-12 03:42:32 +0800

    try

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-
+// $routes->get('/', 'Home::index');
 $routes->get('user/(:num)', 'UserController::getById/$1');
 $routes->get('user', 'UserController::showUser');
 $routes->post('user', 'UserController::createUser');
